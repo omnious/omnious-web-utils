9d0bc1b728b0318cbebee0f363d126507dbd0a65
NoahLuftYang <ykd1217@gmail.com>
2018-02-26 18:55:08 +0900

    [webpack] Add image loader for webpack

--- scripts/build.js
+++
@@ -1,9 +0,0 @@
-// Global import
-const ora = require('ora');
-const clearConsole = require('react-dev-utils/clearConsole');
-const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages');
-const webpack = require('webpack');
-
-// clearConsole();
-// logger.start(`Starting build in ${env} mode`);
-
