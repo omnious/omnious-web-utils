4dd004d5e10cf9b1e99062ba998f07582e4768f9
NoahLuftYang <ykd1217@gmail.com>
2018-05-25 11:50:45 +0900

    Tile extend update

--- packages/ui/src/Input/Input.tsx
+++ packages/ui/src/Input/Input.tsx
@@ -6,6 +6,6 @@ import { StyledLabel } from './styles';
 export const Input: any = ({ disabled, name, title, type, ...props }: any): JSX.Element => (
   <StyledLabel disabled={disabled} htmlFor={name}>
     <span>{title}</span>
-    <input disabled={disabled} name={name} type={type || 'text'} {...props} />
+    <input id={name} disabled={disabled} name={name} type={type || 'text'} {...props} />
   </StyledLabel>
 );
--- packages/ui/src/Tile/Tile.tsx
+++ packages/ui/src/Tile/Tile.tsx
@@ -4,4 +4,6 @@ import * as React from 'react';
 // Local import
 import { StyledTile } from './styles';
 
-export const Tile: any = ({ children }: any): JSX.Element => <StyledTile>{children}</StyledTile>;
+export const Tile: any = ({ children, ...options }: any): JSX.Element => (
+  <StyledTile {...options}>{children}</StyledTile>
+);
