a509dc651a92beaf9304499ee09dd747b4bbce80
NoahLuftYang <ykd1217@gmail.com>
2018-07-17 15:29:16 +0900

    Webpack module alias

--- packages/webpack/webpack.config.common.js
+++ packages/webpack/webpack.config.common.js
@@ -4,13 +4,12 @@
 
 // Global import
 const DotenvPlugin = require('dotenv-webpack');
-// const InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
+const { resolve } = require('path');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
 const { IgnorePlugin } = require('webpack');
 
 // Local import
-// const { CDN_URL, GOOGLE_ID, FACEBOOK_ID } = require('./config/env');
 const { packageJson, srcDir } = require('./config/path');
 
 module.exports = {
@@ -44,13 +43,17 @@ module.exports = {
   },
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
-    alias: {},
+    alias: {
+      '@components': resolve(srcDir, 'components'),
+      '@containers': resolve(srcDir, 'containers'),
+      '@hoc': resolve(srcDir, 'hoc'),
+      '@utils': resolve(srcDir, 'utils')
+    },
     plugins: [new ModuleScopePlugin(srcDir, [packageJson])]
   },
   plugins: [
     new DotenvPlugin({ systemvars: true }),
     new IgnorePlugin(/^\.\/locale$/, /moment$/),
-    // new InterpolateHtmlPlugin({ CDN_URL, GOOGLE_ID, FACEBOOK_ID }),
     new StyleLintPlugin()
   ]
 };
