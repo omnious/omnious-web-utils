5808e83f5ceef90db3ff28cf659661903b1952a5
NoahLuftYang <ykd1217@gmail.com>
2018-07-23 15:24:32 +0900

    Reset build

--- packages/reset/postcss.config.js
+++ packages/reset/postcss.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  // syntax: 'postcss-scss',
+  syntax: 'postcss-scss',
   plugins: [
     require('postcss-partial-import')({ prefix: '_' }),
     require('autoprefixer'),
-    // require('postcss-strip-inline-comments'),
-    require('postcss-nested')
-    // require('postcss-simple-vars')
-    // require('cssnano')
+    require('postcss-strip-inline-comments'),
+    require('postcss-nested'),
+    require('postcss-simple-vars'),
+    require('cssnano')
   ]
 };
